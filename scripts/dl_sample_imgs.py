14048caf66d1250e05d4c90f3cfd163a40cd00e9
Surface Windows <shepherdjosh4@gmail.com>
2018-05-01 11:09:44 -0700

    Prettified download script

--- scripts/dl_sample_imgs.py
+++ scripts/dl_sample_imgs.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 
 img_dir = '../sample_imgs/'
@@ -5,26 +6,47 @@ tmp_zip_pathname = os.path.join(img_dir,'tmp_imgs.zip')
 
 def dl_img_zip():
     import requests
-    img_album_url = 'http://imgur.com/a/SOIUE/zip'
-    r = requests.get(url, stream=True)
 
+    print('Downloading sample images zip...')
+
+    img_album_url = 'http://imgur.com/a/SOIUE/zip'
+    chunk_size = 1024*1024
+
+    r = requests.head(img_album_url)
+    file_size = int(r.headers['content-length'])
+
+    r = requests.get(img_album_url, stream=True)
+
+    fmt_file_size = '{:.2f}'.format(file_size/(1024*1024))
+
+    file_index = 0
     with open(tmp_zip_pathname, 'wb') as fd:
         for chunk in r.iter_content(chunk_size):
+            fmt_file_index = '{:.2f}'.format(file_index/(1024*1024))
+            print(fmt_file_index+'/'+fmt_file_size+' MB', end='\r')
             fd.write(chunk)
+            file_index += chunk_size
+    print(fmt_file_size+'/'+fmt_file_size+' MB')
 
-    print('Successfully downloaded sample images zip.')
+    print('done')
 
 def extract_sample_imgs():
     import zipfile
+
+    print('Extracting sample images... ', end='\r')
+
     with zipfile.ZipFile(tmp_zip_pathname, 'r') as zip_ref:
         zip_ref.extractall(img_dir)
 
-    print('Successfully extracted sample images.')
+    print('Extracting sample images... done')
 
 def rm_tmp_zip():
+    print('Removing temporary zip file... ', end='\r')
+
     os.remove(tmp_zip_pathname)
 
-    print('Successfully removed temporary zip file.')
+    print('Removing temporary zip file... done')
+
 if __name__ == '__main__':
     dl_img_zip()
     extract_sample_imgs()
