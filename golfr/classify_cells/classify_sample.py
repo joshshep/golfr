25f685cef7e3733105def8a4160f0320de8915f0
joshshep <joshua.adam.shepherd@gmail.com>
2018-06-21 19:51:18 -0700

    Added python3 compatibility

--- golfr/classify_cells/classify_sample.py
+++ golfr/classify_cells/classify_sample.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+from __future__ import print_function
 import cv2
 import numpy as np
 import keras
@@ -27,7 +28,7 @@ def classify_digit(digit_fname, model_fname=DFLT_MODEL_PATH):
     ############################################
     ## load sample image
     img = cv2.imread(digit_fname)
-    print 'orig shape:',img.shape
+    print('orig shape:',img.shape)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
@@ -56,9 +57,9 @@ def classify_digit(digit_fname, model_fname=DFLT_MODEL_PATH):
     image, contours, _ = cv2.findContours(contoured, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnt = contours[0]
     x,y,w,h = cv2.boundingRect(cnt)
-    print 'x={}, y={}, w={}, h={}'.format(x,y,w,h)
+    print('x={}, y={}, w={}, h={}'.format(x,y,w,h))
     img = img[y:y+h,x:x+w]
-    print 'cropped shape:',img.shape
+    print('cropped shape:',img.shape)
 
     cv2.imwrite('3.png',img)
 
@@ -68,17 +69,17 @@ def classify_digit(digit_fname, model_fname=DFLT_MODEL_PATH):
     pad_right = 0
     if h > w:
         diff = h - w
-        half_diff = diff/2
+        half_diff = int(diff/2 + 0.5)
         pad_left += half_diff
         pad_right += diff - half_diff
     elif w > h:
         diff = w - h
-        half_diff = diff/2
+        half_diff = int(diff/2 +0.5)
         pad_top += half_diff
         pad_bot += diff - half_diff
 
     img = cv2.copyMakeBorder(img, pad_top,pad_bot,pad_left,pad_right, cv2.BORDER_CONSTANT, value=(0))
-    print 'padded shape:',img.shape
+    print('padded shape:',img.shape)
     cv2.imwrite('4.png',img)
     pad_top = 4
     pad_bot = 4
@@ -97,7 +98,7 @@ def classify_digit(digit_fname, model_fname=DFLT_MODEL_PATH):
 
     img = np.expand_dims(img, 2)
     img = np.expand_dims(img, 0)
-    print 'final shape:',img.shape
+    print('final shape:',img.shape)
 
     ########################################3
     ## print text version
@@ -105,8 +106,8 @@ def classify_digit(digit_fname, model_fname=DFLT_MODEL_PATH):
     for j in range(28):
         for i in range(28):
             #print np.array_repr(img.reshape(28,28)[i]).replace('\n','')
-            print '{:>4}'.format(int(255*img.reshape(28,28)[j][i])),
-        print
+            print('{:>4}'.format(int(255*img.reshape(28,28)[j][i])), end='')
+        print()
     #ret,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 
     ##################################33
@@ -117,7 +118,7 @@ def classify_digit(digit_fname, model_fname=DFLT_MODEL_PATH):
     #np.set_printoptions(precision=4)
     np.set_printoptions(suppress=True)
 
-    print x
+    print(x)
     digit_val = np.argmax(x)
     print(digit_val)
     return digit_val
