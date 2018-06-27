7e6c5b02e4e02166d87631bf34a5f556e8da4d33
joshshep <joshua.adam.shepherd@gmail.com>
2018-06-27 07:00:37 -0700

    Added CNN training test

--- tests/test_classify_cells.py
+++ tests/test_classify_cells.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 from __future__ import print_function
-import golfr
 from golfr.classify_cells.classify_sample import classify_digit
 
 from os import listdir
---
+++ tests/test_train_CNN.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+from __future__ import print_function
+from golfr.train_CNN.mnist_cnn import gen_digit_classifier
+
+import sys, traceback
+
+def test_train_CNN():
+    try:
+        gen_digit_classifier(epochs=50)
+    except:
+        print('Exception: couldn\'t classify {}'.format(fname))
+        print ('-'*60)
+        traceback.print_exc(file=sys.stdout)
+        print ('-'*60)
+        print('test FAILED')
+        return False
+    print('test PASSED')
+    return True
+
+if __name__ == '__main__':
+    test_train_CNN()
